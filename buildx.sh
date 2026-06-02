fae21bb56892fd9440dd9e1b85445683ba1ff00f
cym1102 <cym1102@qq.com>
2026-06-02 11:00:35 +0800

    1

--- buildx.sh
+++ buildx.sh
@@ -33,8 +33,8 @@ docker buildx build \
     --cache-from "type=local,src=/tmp/.buildx-cache" \
     --cache-to "type=local,dest=/tmp/.buildx-cache" \
     --platform "$arch" \
-    --tag registry.cn-hangzhou.aliyuncs.com/cym19871102/nginxwebui:${ver} \
-    --tag registry.cn-hangzhou.aliyuncs.com/cym19871102/nginxwebui:latest \
+    --tag cym1102/nginxwebui:${ver} \
+    --tag cym1102/nginxwebui:latest \
     --push \
     .
 
